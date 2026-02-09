c7156502e7932acb5df8bc47eb5239a7be3cf876
santhosh <san10thoshs@gmail.com>
2026-02-09 12:10:54 +0530

    hero section compleated

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -12,9 +12,10 @@ import { RouterModule } from '@angular/router';
 export class HeroComponent {
   // Config for the floating glass icons
   floatingIcons = [
-    { icon: 'analytics', top: '15%', left: '10%', delay: '0s' },
-    { icon: 'verified_user', top: '25%', right: '15%', delay: '1s' },
-    { icon: 'cloud_upload', top: '55%', left: '18%', delay: '2s' },
-    { icon: 'assignment', top: '45%', right: '8%', delay: '1.5s' }
+    { icon: 'analytics', top: '10%', left: '18%', delay: '0.12s' },
+    { icon: 'verified_user', top: '15%', right: '10%', delay: '0.5s' },
+    { icon: 'cloud_upload', top: '40%', left: '22%', delay: '1.5s' },
+    { icon: 'qr_code', top: '25%', left: '10%', delay: '2s' },
+    { icon: 'enhanced_encryption', top: '40%', right: '18%', delay: '1.5s' }
   ];
 }
